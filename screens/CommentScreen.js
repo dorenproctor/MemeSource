73f0defb524f3e542e09513bd594aa086103c441
Doren Proctor <dorenproctor@gmail.com>
2018-07-13 01:18:45 -0700

    Moved comment input to top of screen. Started submitComment()

--- screens/CommentScreen.js
+++ screens/CommentScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TextInput, KeyboardAvoidingView, Dimensions } from 'react-native'
 import Footer from '../components/Footer'
 
 export default class SingleImageScreen extends React.Component {
@@ -14,6 +14,7 @@ export default class SingleImageScreen extends React.Component {
       user: this.props.navigation.state.params.user,
       commentInfo: null,
       turnModalOn: props.navigation.state.params.turnModalOn,
+      commentBox: "",
     }
   }
 
@@ -38,29 +39,73 @@ export default class SingleImageScreen extends React.Component {
     this.getComments()
   }
 
+  submitComment() {
+    if (!this.state.commentBox) {
+      return
+    }
+    if (!this.user) {
+      alert("You must be signed in to comments")
+      return
+    }
+    fetch('http://ec2-18-188-44-41.us-east-2.compute.amazonaws.com/downvoteImage', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: user,
+        imageId: currentNumber,
+        content: this.state.commentBox
+      })
+    }).then((response) => { return response.json() })
+      .then((response) => {
+        if (response.statusCode == 200) {
+          // alert(JSON.stringify(response.content))
+          this.setState({ imageInfo: response.content })
+        } else (
+          alert(response.message)
+        )
+    })
+    //curl -i -X POST -H 'Content-Type: application/json' -d '{"imageId": "0", "content": "Generic comment.", "user": "someUsername"}' http://ec2-18-188-44-41.us-east-2.compute.amazonaws.com/postComment
+  }
+
   render() {
-    const { commentInfo } = this.state
+    const { commentInfo, commentBox } = this.state
     const { goBack } = this.props.navigation
 
-    const footerButtons = [
-      {"title": "↶", "action": () => goBack(null)},
-      {"title": "💬", "action": () => null},
-      {"title": "", "action": () => null},
-      {"title": "", "action": () => null},
-    ]
     const styles = StyleSheet.create({
       textContainer: {
 
       },
+      commentBox: {
+        // position: 'relative',
+        // bottom: Dimensions.get('window').height- 50,
+      }
     })
+
+    const footerButtons = [
+      {"title": "↶", "action": () => goBack(null)},
+      {"title": "💬", "action": () => this.myInput.focus()},
+      {"title": "", "action": () => null},
+      {"title": "Submit Comment", "action": () => null},
+    ]
     const comments = !commentInfo ? null : commentInfo.map((obj, i) => {
       return (
-        <Text style={styles.text} key={i}>{obj.content}</Text>
+        <Text style={styles.text} key={i}>{obj.user+": "+obj.content}</Text>
       )
     })
+
     
     return (
       <View>
+        <TextInput 
+          onChangeText={(text) => this.setState({commentBox: text})}
+          style={styles.commentBox}
+          value={commentBox}
+          underlineColorAndroid='transparent'
+          multiline={true}
+          numberOfLine={10}
+          ref={input => this.myInput = input} />
         <ScrollView>
           {comments}
         </ScrollView>
